02e19a53bbed3c16ea5ead98de57e1d973a13080
deli-ght <minjju0660@gmail.com>
2021-12-27 17:10:22 +0900

    ✨  add navigation

---
+++ futurama/src/components/layouts/Navigation.tsx
@@ -0,0 +1,64 @@
+import Link from "next/link"
+import { ROUTES } from "../../constants"
+import styled from "@emotion/styled"
+import { css } from "@emotion/react"
+
+interface ROUTE {
+  ID: number
+  PATH: string
+  LABEL: string
+}
+
+export const Navigation = () => {
+  return (
+    <Title>
+      <Link href={"/"} passHref>
+        <LogoImg background={"/symbol.png"} />
+      </Link>
+      <nav>
+        <Navlists>
+          {ROUTES.map((routeObject: ROUTE) => {
+            return (
+              <Navlist key={routeObject.ID}>
+                <Link href={routeObject.PATH}>
+                  <a>{routeObject.LABEL}</a>
+                </Link>
+              </Navlist>
+            )
+          })}
+        </Navlists>
+      </nav>
+    </Title>
+  )
+}
+
+const Title = styled.div`
+  text-align: center;
+`
+type logo = {
+  background: string
+}
+
+const logoimg = (props: logo) => css`
+  background-image: url(${props.background});
+  background-position: center center;
+  background-size: contain;
+  background-repeat: no-repeat;
+`
+
+const LogoImg = styled.a`
+  ${logoimg}
+  display: inline-block;
+  width: 160px;
+  height: 100px;
+`
+const Navlists = styled.ul`
+  margin: 10px;
+`
+
+const Navlist = styled.li`
+  display: inline-block;
+  & + & {
+    margin-left: 10px;
+  }
+`
--- futurama/src/constants/index.ts
+++ futurama/src/constants/index.ts
@@ -3,6 +3,35 @@ export const FUTURAMA_API_ENDPOINT = "https://api.sampleapis.com/futurama/"
 type ColorType = {
   [key: string]: string
 }
+
+export const ROUTES = [
+  {
+    ID: 0,
+    PATH: "/cast",
+    LABEL: "CAST",
+  },
+  { ID: 1, PATH: "/characters", LABEL: "CHARACTERS" },
+  {
+    ID: 2,
+    PATH: "/episodes",
+    LABEL: "EPISODES",
+  },
+  {
+    ID: 3,
+    PATH: "/info",
+    LABEL: "INFO",
+  },
+  {
+    ID: 4,
+    PATH: "/inventory",
+    LABEL: "INVENTORY",
+  },
+  {
+    ID: 5,
+    PATH: "/questions",
+    LABEL: "QUESTIONS",
+  },
+]
 export const COLOR: ColorType = {
   purple: "#a5aada",
   mint: "#34aeac",
