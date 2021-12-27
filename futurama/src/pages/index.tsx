be52d8552c32b43626e68b0517400d1e6cbcb127
deli-ght <minjju0660@gmail.com>
2021-12-28 01:35:52 +0900

    💄 put img in main

--- futurama/src/pages/index.tsx
+++ futurama/src/pages/index.tsx
@@ -1,7 +1,23 @@
+import styled from "@emotion/styled"
 import type { NextPage } from "next"
+import Image from "next/image"
 
 const Home: NextPage = () => {
-  return <div></div>
+  return (
+    <Center>
+      <Image
+        src="/futurama.jpeg"
+        alt="futurama"
+        width={350}
+        height={500}
+      ></Image>
+    </Center>
+  )
 }
 
 export default Home
+
+const Center = styled.main`
+  padding: 30px;
+  text-align: center;
+`
